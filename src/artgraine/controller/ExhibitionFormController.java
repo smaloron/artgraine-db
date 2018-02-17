4816a285ba75418d26565b4917bc08c993edf62b
Sebastien Maloron <contact@smaloron.eu>
2018-02-17 14:44:44 +0100

    Gestion de la saisie des dates d'expo et calcul de la durée d'une exposition

--- src/artgraine/controller/ExhibitionFormController.java
+++ src/artgraine/controller/ExhibitionFormController.java
@@ -6,16 +6,21 @@ import artgraine.model.ExhibitionDAO;
 import artgraine.model.SculptureDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ResourceBundle;
 
-public class ExhibitionFormController extends AbstractController{
+public class ExhibitionFormController extends AbstractController implements Initializable{
 
 
     @FXML public Label formTitleLabel;
@@ -24,6 +29,7 @@ public class ExhibitionFormController extends AbstractController{
     @FXML public DatePicker endDatePicker;
     @FXML public DatePicker departureDatePicker;
     @FXML public DatePicker returnDatePicker;
+    @FXML public Label durationLabel;
 
     private Exhibition exhibition;
     private ExhibitionDAO exhibitionDAO;
@@ -58,6 +64,38 @@ public class ExhibitionFormController extends AbstractController{
             e.printStackTrace();
         }
 
+
+    }
+
+    private void setDuration(LocalDate fromDate, LocalDate toDate){
+        Long duration = 0L;
+        if(departureDatePicker.getValue() != null){
+            duration = ChronoUnit.DAYS.between(fromDate, toDate);
+        }
+        String dayLabel = (duration>1)?" jours":" jour";
+        durationLabel.setText(duration + dayLabel);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        startDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if(departureDatePicker.getValue() == null || departureDatePicker.getValue().isAfter(newValue)){
+                departureDatePicker.setValue(newValue);
+            }
+            if(endDatePicker.getValue() == null || endDatePicker.getValue().isBefore(newValue)){
+                endDatePicker.setValue(newValue.plusDays(7));
+            }
+        });
+
+        endDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if(returnDatePicker.getValue() == null || returnDatePicker.getValue().isBefore(newValue)){
+                returnDatePicker.setValue(newValue);
+            }
+        });
+
+        returnDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+            setDuration(departureDatePicker.getValue(), newValue);
+        });
 
     }
 }
